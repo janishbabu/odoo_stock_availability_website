46f5133af13a02f6ca20ec2eb6f0dd9448545aca
janishbabuek <janishbabuek@gmail.com>
2023-03-24 17:17:59 +0530

    project complted

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Stock Availability",
+    'version': "16.0.1.0.0",
+    'sequence': -11,
+    'summary': 'sale order',
+    'author': 'CYBROSYS',
+    'installable': True,
+    'application': True,
+    'depends': ['base','website_sale'],
+
+    'data': ['views/stock_availability.xml'
+    ]
+
+}
+
